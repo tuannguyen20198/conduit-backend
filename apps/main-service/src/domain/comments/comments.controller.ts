44b0c057a870843a475db07e77df2a7b2616df00
matocdo2018 <lynguyenthanhtuan@gmail.com>
2025-03-05 14:42:41 +0700

    code comment done

--- apps/main-service/src/domain/comments/comments.controller.ts
+++ apps/main-service/src/domain/comments/comments.controller.ts
@@ -4,6 +4,7 @@ import { CreateCommentDto } from './dto/create-comment.dto';
 import { UpdateCommentDto } from './dto/update-comment.dto';
 import { Identity } from '../auth/identity.decorator';
 import { AuthGuard } from '../auth/auth.guard';
+import { ResponseMessage } from '@nnpp/decorators/message.decorator';
 
 @Controller('article')
 export class CommentsController {
@@ -18,5 +19,34 @@ export class CommentsController {
     @Identity() user
   ) {
     return this.commentsService.create(slug, createCommentDto.comment.body, user.id);
+  }
+  @Get(":slug/comments")
+  async findAll() {
+    return this.commentsService.findAll();
+  }
+  @UseGuards(AuthGuard) // Chỉ cho phép user đã đăng nhập xóa comment
+  @Delete(':slug/comments/:id')
+  async deleteComment(
+    @Param('slug') slug: string,
+    @Param('id') commentId: string,
+    @Identity() user // Lấy thông tin user từ request
+  ) {
+    return this.commentsService.deleteComment(slug, parseInt(commentId), user.id);
+  }
+
+  @UseGuards(AuthGuard) // Chỉ cho phép user đã đăng nhập thêm favorite
+  @ResponseMessage("Favorite Success")
+  @Post(':slug/favorite')
+  async favoriteArticle(
+    @Param('slug') slug: string,
+    @Identity() user // Lấy thông tin user từ request
+  ) {
+    return this.commentsService.favoriteArticle(slug, user.id);
+  }
+  @UseGuards(AuthGuard)
+  @ResponseMessage("UnFavorite Success")
+  @Delete(':slug/favorite')
+  async unfavoriteArticle(@Param('slug') slug: string, @Identity() user) {
+    return this.commentsService.unfavoriteArticle(slug, user.id);
   }
 }
