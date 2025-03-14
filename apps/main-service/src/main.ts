763bb4a559ad724d36017fbd964278248e2d9d94
matocdo2018 <lynguyenthanhtuan@gmail.com>
2025-03-15 06:53:21 +0700

    update swagger

--- apps/main-service/src/main.ts
+++ apps/main-service/src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { setupApiServer } from '@nnpp/setup/setup-api-server';
 import { AppModule } from './app.module';
 import { config } from './config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { writeFileSync } from 'fs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -19,5 +21,18 @@ async function bootstrap() {
       version: '1.0',
     },
   });
+  //  Định nghĩa cấu hình Swagger
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Conduit APIs')
+    .setDescription('Conduit API description')
+    .setVersion('1.0')
+    .addBearerAuth() // Nếu API cần xác thực
+    .build();
+
+  //  Tạo tài liệu Swagger chính xác
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api/', app, swaggerDocument);
+  //  Ghi file JSON
+  writeFileSync('./swagger.json', JSON.stringify(swaggerDocument, null, 2));
 }
 bootstrap();
