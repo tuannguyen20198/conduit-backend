15bb53e5b9de8cf9654895d64bd3c16f50f63e8e
matocdo2018 <lynguyenthanhtuan@gmail.com>
2025-04-02 06:29:05 +0700

    add router and delete ErrorCoundary

--- conduit-fe/src/App.tsx
+++ conduit-fe/src/App.tsx
@@ -1,11 +1,8 @@
 import AppRouter from "@/router/route";
-import ErrorBoundary from "./provider/ErrorBoundary";
 
 const App = () => {
   return (
-    <ErrorBoundary>
       <AppRouter />
-    </ErrorBoundary>
   );
 };
 
